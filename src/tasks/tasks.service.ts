04558aefc31efe129d327226e5e93f58e840297b
Adnan Husain <husainadnan15@gmail.com>
2020-05-10 14:40:09 +0530

    app init with base controller and service

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { uniqueId } from 'lodash'
+
+@Injectable()
+export class TasksService {
+    private tasks: Task[] = [];
+
+    getAllTasks(): Task[] {
+        return this.tasks;
+    }
+
+    createTask(title: string, description: string) {
+        const task: Task = {
+            id: uniqueId('My_Tasks_'),
+            title,
+            description,
+            status: TaskStatus.OPEN,
+        };
+        this.tasks.push(task);
+        return task;
+    }
+}
